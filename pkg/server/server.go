c118a0022928dfee40b31fcfbc4ce651d862362a
HashedDan <georgedanielmangum@gmail.com>
2019-03-29 10:03:36 -0500

    Typo corrections and comments for server pkg

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,31 +6,36 @@ import (
 	"net/http"
 )
 
+// Server is interface defining methods required of the server
 type Server interface {
 	Start() error
 	Stop() error
 }
 
+// Srv is an implementation of the Server interface
 type Srv struct {
 	port   string
 	server *http.Server
 }
 
-func NewHttpServer(port string) *Srv {
+// NewHTTPServer returns a Srv instance
+func NewHTTPServer(port string) *Srv {
 	return &Srv{
 		port:   port,
 		server: &http.Server{Addr: ":" + port},
 	}
 }
 
+// Start causes the server to listen for tcp connections on specified port
 func (s *Srv) Start() error {
 	if err := s.server.ListenAndServe(); err != http.ErrServerClosed {
-		log.Fatalf("Server unexpectedly shutdodn: %s", err)
+		log.Fatalf("Server unexpectedly shutdown: %s", err)
 	}
 
 	return nil
 }
 
+// Stop causes the server to stop listening for tcp connections
 func (s *Srv) Stop() error {
 	if err := s.server.Shutdown(context.TODO()); err != nil {
 		return err
